5c438ab35bcdcd5c28d488e6bf6dec5244a6fbb9
rafaballerini <rafaella_ballerini@hotmail.com>
2021-12-17 17:57:44 -0300

    comandos basicos e criação do slash command (não funcional)

---
+++ index.js
@@ -0,0 +1,56 @@
+import { Client, Intents } from "discord.js";
+import techs from "./techs.json";
+import { REST } from "@discordjs/rest";
+import { Routes } from "discord-api-types/v9";
+import "dotenv/config";
+
+const rest = new REST({ version: "9" }).setToken(process.env.token);
+
+const commands = [
+  {
+    name: "docs",
+    description: "Acesse a documentação da sua tech favorita!",
+  },
+];
+
+const client = new Client({
+  intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
+});
+
+client.on("ready", () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on("messageCreate", (message) => {
+  let tecnologias = Object.keys(techs);
+
+  if (tecnologias.includes(message.content)) {
+    message.channel.send(
+      `📚 Estude ${message.content} com a documentação: ${
+        techs[message.content]
+      } 📚`
+    );
+  }
+});
+
+(async () => {
+  try {
+    console.log("Started refreshing application (/) commands.");
+
+    await rest.put(
+      Routes.applicationGuildCommands(
+        "921428827895398400",
+        "836004917973614662"
+      ),
+      {
+        body: commands,
+      }
+    );
+
+    console.log("Successfully reloaded application (/) commands.");
+  } catch (error) {
+    console.error(error);
+  }
+})();
+
+client.login(process.env.token);
---
+++ readme.md
@@ -0,0 +1,26 @@
+# Discord Bot com Discord.js
+
+## Configurando o ambiente de desenvolvimento
+
+1. `npm install discord.js`
+2. `npm install discord.js @discordjs/rest discord-api-types`
+
+## Criando o token do bot para interação com a API do Discord
+
+1. [Developers Potal](https://discord.com/developers/docs/intro)
+2. Clicar em `Applications` e criar uma nova, completando com a imagem, nome, descrição e tags que quiser
+3. Vai na aba de `Bot` e crie um novo, também com a personalização que desejar 
+4. Copie o token do bot e cole no código
+5. Crie as [permissões necessárias](https://discordapi.com/permissions.html) para esse bot interagir no seu servidor (recomendo usar um servidor teste)
+Ele pedirá um ID, que você encontra na primeira página de `Application`
+
+## Rodar código
+
+`node --experimental-json-modules index.js`
+`node index.js`
+
+## Acessos
+
+![slash commands pt 1](https://cdn.discordapp.com/attachments/921446556832530443/921471129204424744/unknown.png)
+![slash commands pt 2](https://cdn.discordapp.com/attachments/921446556832530443/921471338907041792/unknown.png)
+![slash commands pt 3](https://cdn.discordapp.com/attachments/921446556832530443/921471610953801798/unknown.png)
